0f2e8f6b829a30a7ec19099413ecb9503f222eae
tuxhar <43926459+tuxhar@users.noreply.github.com>
2024-02-24 09:34:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+# https://tea.xyz/what-is-this-file
+---
+version: 1.0.0
+codeOwners:
+  - '0x5AC4018337662b0c5Da935fEE6e2807F9120D753'
+quorum: 1
+
+
+<?php
+
+// Comment these lines to hide errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'includes/config.php';
+require 'includes/functions.php';
+
+init();
